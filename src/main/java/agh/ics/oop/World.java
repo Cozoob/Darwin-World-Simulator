74ba1f14db6db075aae3b384b9d05dbaa6df23ec
Marcin Kozub <kozubmarcin10@gmail.com>
2021-10-16 12:48:06 +0200

    Modify static method run

--- src/main/java/agh/ics/oop/World.java
+++ src/main/java/agh/ics/oop/World.java
@@ -3,12 +3,19 @@ import static java.lang.System.out;
 
 public class World {
     public static void main(String[] args){
+        String[] moves = {"forward", "left"};
         out.print("system wystartował\n");
-        run();
+        run(moves);
         out.print("system zakończył działanie");
     }
 
-    public static void run(){
+    public static void run(String[] strArray) {
         out.print("zwierze idzie do przodu\n");
+        for(int i = 0; i < strArray.length - 1; i++) {
+            out.print(strArray[i]);
+            out.print(", ");
+        }
+        out.print(strArray[strArray.length - 1]);
+        out.print("\n");
     }
 }
