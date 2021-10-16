c3016bc6727fe2900b37333a33f017a8e1e668b5
Marcin Kozub <kozubmarcin10@gmail.com>
2021-10-16 14:19:07 +0200

    Add input arguments and modify enum Direction

--- src/main/java/agh/ics/oop/World.java
+++ src/main/java/agh/ics/oop/World.java
@@ -3,8 +3,23 @@ import static java.lang.System.out;
 
 public class World {
     public static void main(String[] args){
-        // String[] moves = {"f", "f", "r", "l", "o", "c"};
-        Direction[] moves = {Direction.FORWARD, Direction.FORWARD, Direction.RIGHT, Direction.LEFT};
+        Direction moves[];
+        moves = new Direction[args.length];
+        int idx = 0;
+        for(String arg : args){
+            Direction move = switch (arg){
+                case "f" -> Direction.FORWARD;
+                case "l" -> Direction.LEFT;
+                case "r" -> Direction.RIGHT;
+                case "b" -> Direction.BACKWARD;
+                default -> Direction.NONE;
+            };
+            moves[idx] = move;
+            idx++;
+        }
+
+
+        // Direction[] moves = {Direction.FORWARD, Direction.FORWARD, Direction.RIGHT, Direction.LEFT};
         out.print("Start\n");
         run(moves);
         out.print("Stop");
@@ -17,6 +32,7 @@ public class World {
                 case LEFT -> "Zwierzak skręca w lewo\n";
                 case RIGHT -> "Zwierzak skręca w prawo\n";
                 case FORWARD -> "Zwierzak idzie do przodu\n";
+                case NONE -> "NIGDZIE\n";
             };
             out.print(message);
         }
