e0d5d9d707991facf9b167a9410a2d2d01ee5aec
Marcin Kozub <kozubmarcin10@gmail.com>
2021-10-16 12:07:53 +0200

    New static method

--- src/main/java/agh/ics/oop/World.java
+++ src/main/java/agh/ics/oop/World.java
@@ -4,6 +4,11 @@ import static java.lang.System.out;
 public class World {
     public static void main(String[] args){
         out.print("system wystartował\n");
+        run();
         out.print("system zakończył działanie");
+    }
+
+    public static void run(){
+        out.print("zwierze idzie do przodu\n");
     }
 }
