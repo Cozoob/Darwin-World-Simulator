2976a44386716de61b896db516ba44f0230937d2
Marcin Kozub <kozubmarcin10@gmail.com>
2021-10-16 12:00:03 +0200

    My first Java program

---
+++ src/main/java/agh/ics/oop/World.java
@@ -0,0 +1,9 @@
+package agh.ics.oop;
+import static java.lang.System.out;
+
+public class World {
+    public static void main(String[] args){
+        out.print("system wystartował\n");
+        out.print("system zakończył działanie");
+    }
+}
