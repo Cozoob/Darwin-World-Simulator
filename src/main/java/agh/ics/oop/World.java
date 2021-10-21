b15247f0120fc7c38dc3139e65458345f928bda4
Marcin Kozub <kozubmarcin10@gmail.com>
2021-10-21 17:17:23 +0200

    create vector2d class

--- src/main/java/agh/ics/oop/World.java
+++ src/main/java/agh/ics/oop/World.java
@@ -1,40 +1,24 @@
 package agh.ics.oop;
 import static java.lang.System.out;
+/* napisz hashcode */
 
-public class World {
-    public static void main(String[] args){
-        Direction moves[];
-        moves = new Direction[args.length];
-        int idx = 0;
-        for(String arg : args){
-            Direction move = switch (arg){
-                case "f" -> Direction.FORWARD;
-                case "l" -> Direction.LEFT;
-                case "r" -> Direction.RIGHT;
-                case "b" -> Direction.BACKWARD;
-                default -> Direction.NONE;
-            };
-            moves[idx] = move;
-            idx++;
-        }
+class Vector2d {
+    public final int x;
+    public final int y;
 
-
-        // Direction[] moves = {Direction.FORWARD, Direction.FORWARD, Direction.RIGHT, Direction.LEFT};
-        out.print("Start\n");
-        run(moves);
-        out.print("Stop");
+    public Vector2d(int x, int y){
+        this.x = x;
+        this.y= y;
     }
 
-    public static void run(Direction[] moves) {
-        for(Direction move : moves){
-            String message = switch(move){
-                case BACKWARD -> "Zwierzak idzie do tyłu\n";
-                case LEFT -> "Zwierzak skręca w lewo\n";
-                case RIGHT -> "Zwierzak skręca w prawo\n";
-                case FORWARD -> "Zwierzak idzie do przodu\n";
-                case NONE -> "NIGDZIE\n";
-            };
-            out.print(message);
-        }
+    public String toString() {
+        return "(" + Integer.toString(this.x) + "," + Integer.toString(this.y) + ")";
+    }
+
+}
+public class World {
+    public static void main(String[] args){
+        Vector2d V = new Vector2d(1, 2);
+        out.print(V.toString());
     }
 }
