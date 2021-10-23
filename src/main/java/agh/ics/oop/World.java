76ef55b4e4cfb8fd6efa887f44ad2d9d91ae2a2e
Marcin Kozub <kozubmarcin10@gmail.com>
2021-10-23 11:28:06 +0200

    Add new methods

--- src/main/java/agh/ics/oop/World.java
+++ src/main/java/agh/ics/oop/World.java
@@ -12,13 +12,74 @@ class Vector2d {
     }
 
     public String toString() {
-        return "(" + Integer.toString(this.x) + "," + Integer.toString(this.y) + ")";
+        return "(" + this.x + "," + this.y + ")";
+    }
+
+    public boolean precedes(Vector2d other){
+        return this.x == other.x && this.y == other.y;
+
+    }
+
+    public boolean follows(Vector2d other){
+        return this.x >= other.x && this.y >= other.y;
+    }
+
+    public Vector2d upperRight(Vector2d other){
+        return new Vector2d(Integer.max(this.x, other.x),Integer.max(this.y, other.y));
+    }
+
+    public Vector2d lowerLeft(Vector2d other){
+        return new Vector2d(Integer.min(this.x, other.x),Integer.min(this.y, other.y));
+    }
+
+    public Vector2d add(Vector2d other){
+        return new Vector2d(this.x + other.x, this.y + other.y);
+    }
+
+    public Vector2d subtract(Vector2d other){
+        return new Vector2d(this.x - other.x, this.y - other.y);
+    }
+
+    // comparing objects
+    public boolean equals(Object other){
+        if (this == other)
+            return true;
+        if (!(other instanceof Vector2d))
+            return false;
+        Vector2d that = (Vector2d) other;
+        return this.x == that.x && this.y == that.y;
+    }
+
+    // comparing objects with hashcode()
+    public boolean equals2(Object other){
+        boolean isHashCodeEquals = this.hashCode() == other.hashCode();
+        if (isHashCodeEquals)
+            return true;
+        if (!(other instanceof Vector2d))
+            return false;
+        Vector2d that = (Vector2d) other;
+        return this.x == that.x && this.y == that.y;
+    }
+
+    public Vector2d opposite(){
+        return new Vector2d(this.y, this.x);
     }
 
 }
 public class World {
     public static void main(String[] args){
-        Vector2d V = new Vector2d(1, 2);
-        out.print(V.toString());
+        Vector2d V1 = new Vector2d(1, 2);
+        // out.print(V1);
+        Vector2d V2 = new Vector2d(1, 2);
+        // out.print(V1.precedes(V2));
+        // out.print(V1.equals(V2));
+//        out.print(V1.equals2(V2) + "\n");
+//        out.print(V1.hashCode() + "\n");
+//        out.print(V2.hashCode());
+        out.print(V1.opposite());
+
     }
+
+
+
 }
