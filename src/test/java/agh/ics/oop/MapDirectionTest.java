a5c86d763c739f243cfd0c5539e536dc5e1dfb4a
Marcin Kozub <kozubmarcin10@gmail.com>
2021-10-23 12:33:30 +0200

    Modify build.gradle

---
+++ src/test/java/agh/ics/oop/MapDirectionTest.java
@@ -0,0 +1,27 @@
+package agh.ics.oop;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+
+public class MapDirectionTest {
+
+    @Test
+    public void textNext(){
+
+    }
+
+//    public boolean testNext(){
+//        MapDirection direction = MapDirection.NORTH;
+//        if (!(direction.next().toString().equals("Wschod")))
+//                return false;
+//        direction = direction.next();
+//        if (!(direction.next().toString().equals("Poludnie")))
+//            return false;
+//        direction = direction.next();
+//        if (!(direction.next().toString().equals("Zachod")))
+//            return false;
+//        direction = direction.next();
+//        return direction.next().toString().equals("Polnoc");
+//    }
+}
