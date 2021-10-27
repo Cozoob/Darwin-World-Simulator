6a0d32ab5fc29b14b1ed90e600ca2ea03f891966
Marcin Kozub <kozubmarcin10@gmail.com>
2021-10-27 12:13:00 +0200

    Done new class Animal

--- src/main/java/agh/ics/oop/Animal.java
+++ src/main/java/agh/ics/oop/Animal.java
@@ -3,53 +3,37 @@ package agh.ics.oop;
 import java.util.Map;
 
 public class Animal {
-    private Vector2d position = new Vector2d(2,2);
-    private MapDirection direction = MapDirection.NORTH;
+    private Vector2d position;
+    private MapDirection mapDirection;
+    private final static Vector2d downLeftCorner = new Vector2d(0,0);
+    private final static Vector2d upRightCorner = new Vector2d(4,4);
+
+    public Animal(){
+        mapDirection = MapDirection.NORTH;
+        position = new Vector2d(2,2);
+    }
 
     public String toString(){
-        return position.toString() + ", " + direction.toString();
+        return position.toString() + ", " + mapDirection.toString();
     }
 
     public void move(MoveDirection direction){
-        if (direction == MoveDirection.RIGHT){
-            this.direction = this.direction.next();
-
-        } else if (direction == MoveDirection.LEFT){
-            this.direction = this.direction.previous();
-
-        } else if (direction == MoveDirection.FORWARD){
-
-            if (this.direction == MapDirection.NORTH && position.y < 4){
-                // do gory
-                position.add(new Vector2d(0, 1));
-            } else if(this.direction == MapDirection.SOUTH && position.y > 0){
-                // w dol
-                position.add(new Vector2d(0, -1));
-            } else if (this.direction == MapDirection.EAST && position.x < 4){
-                // w prawo
-                position.add(new Vector2d(1, 0));
-            } else if (this.direction == MapDirection.WEST && position.x > 0){
-                // w lewo
-                position.add(new Vector2d(-1, 0));
-            }
-        } else if (direction == MoveDirection.BACKWARD){
-
-            if(this.direction == MapDirection.NORTH && position.y > 0){
-                // w dol
-                position.add(new Vector2d(0, -1));
-            } else if (this.direction == MapDirection.SOUTH && position.y < 4){
-                // w gore
-                position.add(new Vector2d(0, 1));
-            } else if (this.direction == MapDirection.EAST && position.x > 0){
-                // w lewo
-                position.add(new Vector2d(-1, 0));
-            } else if (this.direction == MapDirection.WEST && position.x < 4){
-                // w prawo
-                position.add(new Vector2d(1, 0));
-            }
-        }
-
-
+       switch (direction){
+           case FORWARD -> {
+               Vector2d newPosition = this.position.add(this.mapDirection.toUnitVector());
+               if(newPosition.precedes(upRightCorner)){
+                    this.position = newPosition;
+               }
+           }
+           case BACKWARD -> {
+               Vector2d newPosition = this.position.add(this.mapDirection.toUnitVector().opposite());
+               if(newPosition.follows(downLeftCorner)){
+                   this.position = newPosition;
+               }
+           }
+           case LEFT -> this.mapDirection = this.mapDirection.previous();
+           case RIGHT -> this.mapDirection = this.mapDirection.next();
+       }
     }
 
 
